5546729ed42e09b40fa76c06736f034342864adc
jetpack444 <132483736+jetpack444@users.noreply.github.com>
2025-02-07 17:10:24 +0300

    Update Util.java
    Добавил Session factory и конфигурацию для Hibernate

--- PP_1_1_3-4_JDBC/src/main/java/jm/task/core/jdbc/util/Util.java
+++ PP_1_1_3-4_JDBC/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -4,6 +4,12 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+// Привat и сесион фактори
+
 public class Util {
     /* реализуйте настройку соеденения с БД */
 
@@ -13,6 +19,28 @@ public class Util {
     private static final String USER = "root";
     // пароль к базе данных
     private static final String PASSWORD = "root";
+
+    private static SessionFactory sessionFactory;
+
+    static {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
+            configuration.setProperty("hibernate.connection.url", URL);
+            configuration.setProperty("hibernate.connection.username", USER);
+            configuration.setProperty("hibernate.connection.password", PASSWORD);
+            configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+            configuration.setProperty("hibernate.hbm2ddl.auto", "update");
+            configuration.addAnnotatedClass(User.class);
+            sessionFactory = configuration.buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
 
     public static Connection getConnection() {
         try {
